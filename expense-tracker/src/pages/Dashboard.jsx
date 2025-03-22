dd89d605140296ea725ba832cf442e0dbbdb996a
Thundercloud12 <poojarysaritha064@gmail.com>
2025-03-22 19:30:30 +0530

    Update Dashboard.jsx

--- expense-tracker/src/pages/Dashboard.jsx
+++ expense-tracker/src/pages/Dashboard.jsx
@@ -18,7 +18,6 @@ const Dashboard = () => {
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          {/* Sidebar content here */}
           <li><Link to={`/dashboard/${user_id}/overview`}>Overview</Link></li>
           <li><Link to={`/dashboard/${user_id}/expense`}>Expense</Link></li>
         </ul>
