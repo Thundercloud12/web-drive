9b05df767a64cf8b1f2ab768a21499e6bba3d894
SatyamPandey-07 <pandeysatyam1802@gmail.com>
2025-04-06 19:14:09 +0530

    Initial commit with code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+
 import Navbar from './pages/Navbar';
+import Footer from './pages/Footer';
+
+
 import Login from './pages/login';
 import Signup from './pages/signup';
-import HomePage from './pages/HomePage';
 import AdminKey from './pages/AdminLogin';
-import Footer from './pages/Footer';
+
+
+import HomePage from './pages/HomePage';
+
+import Dashboard from './pages/Dashboard';
+
+
+import AdminDashboard from './pages/AdminDashboard';
+import BookAdmin from './pages/BookAdmin';
+import IssueAdmin from './pages/IsssueAdmin';
+import UserList from './pages/UserList';
+import UserVerification from './pages/UserVerification';
+
+
+import Books from './pages/Books';
+import BookUser from './pages/BookUser';
+
 function App() {
   return (
     <>
       <Navbar />
       <div className="pt-20">
         <Routes>
+       
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/admin-login" element={<AdminKey />} />
+
+       
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/book-user" element={<BookUser />} />
+
+     
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/books" element={<BookAdmin />} />
+          <Route path="/admin/issue-requests" element={<IssueAdmin />} />
+          <Route path="/admin/users" element={<UserList />} />
+          <Route path="/admin/user-verification" element={<UserVerification />} />
         </Routes>
       </div>
       <Footer />
