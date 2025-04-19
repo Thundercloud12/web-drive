12e554e85df6cc50f699a1aef327287ea5414e46
Thundercloud12 <poojarysaritha064@gmail.com>
2025-04-19 21:17:06 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,14 +1,13 @@
-// src/utils/axios.js
 import axios from 'axios';
 import  store  from "../store/store";
 import { logout } from '../slices/authSlice';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4300/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
-// ✅ Attach token from localStorage
+
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers["Authorization"] = `Bearer ${token}`;
@@ -17,20 +16,18 @@ instance.interceptors.request.use((config) => {
   return Promise.reject(error);
 });
 
-// ✅ Handle token expiration globally
+
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
     const status = error.response?.status;
 
     if (status === 401 || status === 403) {
-      // Dispatch logout action to Redux
+  
       store.dispatch(logout());
 
-      // Clear localStorage token
       localStorage.removeItem("token");
 
-      // Redirect to login
       window.location.href = "/login";
     }
 
