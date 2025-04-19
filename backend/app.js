95add880eaf2074ad6ba459ddfe273b8b1bdf95d
Thundercloud12 <poojarysaritha064@gmail.com>
2025-04-19 20:48:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,8 @@ import path from "path"
 
 const app = express()
 
-// ✅ CORS must be applied before all routes
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+
+app.use(cors({ origin: ["http://localhost:5173","https://library-management-system-new-final.vercel.app"],credentials: true }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -16,7 +16,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-// ✅ Now routes will have CORS headers
+
 app.use("/api/v1/stats", statRoute)
 app.use("/api/v1/users", userRouter)
 
